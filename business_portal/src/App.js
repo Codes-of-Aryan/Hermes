c990beee686cd6494a36e6c9b737e3e21aaa5136
Pratik Bhattacharjee <pratikbhattacharjee09@gmail.com>
2023-11-18 23:54:49 +0800

    Notification List added, with a local

--- business_portal/src/App.js
+++ business_portal/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import VerticalMenu from "./components/VerticalMenu";
+import NotificationList from "./components/NotificationList";
 
 function App() {
   return (
-    <div>
+    <div className="main-app-window">
       <Header />
       <VerticalMenu />
+      <NotificationList />
     </div>
   );
 }
