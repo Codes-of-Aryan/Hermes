e77e5fcbecaa349ced163b80668e037304185ee4
MarzukhAsjad <marz.asjad00@gmail.com>
2023-11-19 00:02:04 +0800

    Created Cathy

---
+++ Cathy/prompts/cathy-sample-prompts.ts
@@ -0,0 +1,7 @@
+export const prompt = `You are an event and customer request handler service powered by Generative A.I. to sync and personalise the intermodal travel experience of Cathay Pacific customers. `;
+
+export const samplePhrases = [
+  "What are some good transport options to get around Shenzhen?",
+  "I like orange juice, can you ensure it's available on my limousine?",
+  "Should I rent a car or hire a limousine to go around in Sydney?",
+];
---
+++ Cathy/src/components/Welcome.tsx
@@ -0,0 +1,15 @@
+export const Welcome: React.FC = () => (
+  <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
+    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Cathy!</h1>
+    <p>
+      This is some Initial Placeholder text that will be displayed by Cathy or
+      Hermes or whatever bro. I don't know what to put here. Also check out the
+      video below for how Cathy works.
+    </p>
+    <p>
+      <a className="underline" href="https://github.com/ascorbic/daneel#readme">
+        Cathy Video Resources 📽️
+      </a>
+    </p>
+  </div>
+);
